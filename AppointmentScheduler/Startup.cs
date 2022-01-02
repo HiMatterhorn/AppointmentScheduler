86e228bad818cf8d4594dbe0f004ed9ad29302c6
HiMatterhorn <wyp4tomasz@gmail.com>
2022-01-02 19:59:41 +0100

    Tables to control identities created

--- AppointmentScheduler/Startup.cs
+++ AppointmentScheduler/Startup.cs
@@ -22,7 +22,7 @@ namespace AppointmentScheduler
         {
             services.AddControllersWithViews();
             services.AddDbContext<ApplicationDbContext>
-                (options => options.UseSqlServer(Configuration.GetConnectionString("DefaulConnection")));
+                (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
