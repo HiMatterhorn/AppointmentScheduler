5ae5a9cec796c1bae9f92e2c3aa7e6df128a0080
HiMatterhorn <wyp4tomasz@gmail.com>
2022-01-06 12:44:05 +0100

    Assigning the user's role

--- AppointmentScheduler/Models/ViewModels/RegisterViewModel.cs
+++ AppointmentScheduler/Models/ViewModels/RegisterViewModel.cs
@@ -10,6 +10,7 @@ namespace AppointmentScheduler.Models.ViewModels
         [EmailAddress]
         public string Email { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         [StringLength(100, ErrorMessage = "The {0} be at least {2} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
 
