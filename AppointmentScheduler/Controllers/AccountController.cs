fb5dc92cc517569bb64b1cf4096cad5df2c53f7c
HiMatterhorn <wyp4tomasz@gmail.com>
2022-01-06 13:02:31 +0100

    Login controller action

--- AppointmentScheduler/Controllers/AccountController.cs
+++ AppointmentScheduler/Controllers/AccountController.cs
@@ -27,6 +27,22 @@ namespace AppointmentScheduler.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email,model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid login attempt");
+            }
+            return View(model);
+        }
+
         public async Task<IActionResult> Register()
         {
             if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
@@ -37,6 +53,8 @@ namespace AppointmentScheduler.Controllers
             }
             return View();
         }
+
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
