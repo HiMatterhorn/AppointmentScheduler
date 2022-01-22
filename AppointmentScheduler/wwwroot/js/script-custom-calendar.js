c5a0385e0530da7c2a3daa52c4065580a5ddd5b7
HiMatterhorn <wyp4tomasz@gmail.com>
2022-01-22 19:48:15 +0100

    Show modal (JS) popup information

--- AppointmentScheduler/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduler/wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,20 @@
-﻿
+﻿(document).addEventListener('DOMContentLoaded', function () {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+
+            headerToolbar: {
+                left: 'prev,next,today',
+                center: 'title',
+                right: 'timeGridWeek,dayGridMonth,timeGridDay'
+            },
+            selectable: true,
+            select: function (event) {
+                onShowModal(event, null);
+            }
+        });
+        calendar.render();
+});
+
+function onShowModal(obj, isEventDetail) {
+    $("#appointmentInput").modal("show")};
