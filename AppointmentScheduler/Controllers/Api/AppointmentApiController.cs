2849c466e8285e5fbfe0d6eb26c85e179dfde845
HiMatterhorn <wyp4tomasz@gmail.com>
2022-04-11 19:07:43 +0200

    GetCalendarData API

--- AppointmentScheduler/Controllers/Api/AppointmentApiController.cs
+++ AppointmentScheduler/Controllers/Api/AppointmentApiController.cs
@@ -4,6 +4,7 @@ using AppointmentScheduler.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace AppointmentScheduler.Controllers.Api
@@ -50,5 +51,41 @@ namespace AppointmentScheduler.Controllers.Api
             }
             return Ok(commonResponse);
         }
+
+        [HttpGet]
+        [Route("GetCalendarData")]
+        public IActionResult GetCalendarData(string doctorId)
+        {
+            CommonResponse<List<AppointmentVM>> commonResponse = new CommonResponse<List<AppointmentVM>>;
+            try
+            {
+                if (role == Helper.Patient)
+                {
+                    commonResponse.dataenum = _appointmentService.PatientsEventsById(loginUserId);
+                    commonResponse.status = Helper.success_code;
+                }
+
+                else if (role == Helper.Doctor)
+                {
+                    commonResponse.dataenum = _appointmentService.DoctorsEventsById(loginUserId);
+                    commonResponse.status = Helper.success_code;
+                }
+
+                else
+                {
+                    commonResponse.dataenum = _appointmentService.PatientsEventsById(doctorId);
+                    commonResponse.status = Helper.success_code;
+                }
+            }
+            catch (Exception e)
+            {
+
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.failure_code;
+            }
+
+            return Ok(commonResponse);
+        }
+
     }
 }
